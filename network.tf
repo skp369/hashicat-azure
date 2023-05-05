f9978cbe311a422f6083f2ca39200286f1ec11dc
skp <skperumalla999@gmail.comm>
2023-05-05 16:32:49 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,7 @@
+
 module "network" {
   source  = "app.terraform.io/wf/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
